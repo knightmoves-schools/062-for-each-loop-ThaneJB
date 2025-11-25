e9232cf648f11de0e5920dc03006e358b547cabb
Thane <thabay0606@students.maquoketaschools.org>
2025-11-25 14:20:26 -0600

    Should work. Added markAsDone function with .forEach loop inside.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function markAsDone(todos) {
+    const updatedTodos = [];
+    
+    todos.forEach((todo,index) => {
+        updatedTodos[index] = `done  - ${todo}`;
+    });
+    
+    return updatedTodos;
+}
