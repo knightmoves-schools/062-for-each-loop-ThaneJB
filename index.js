5c0873fff8d5d991443d854acf0420017a0f53a9
Thane <thabay0606@students.maquoketaschools.org>
2025-11-25 14:22:15 -0600

    Had a space in the output.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function markAsDone(todos) {
     const updatedTodos = [];
     
     todos.forEach((todo,index) => {
-        updatedTodos[index] = `done  - ${todo}`;
+        updatedTodos[index] = `done - ${todo}`;
     });
-    
+
     return updatedTodos;
 }
